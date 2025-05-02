70479dac9d85feca5f96a4459c112eb1e2f04b3d
Aboli1512 <malshikareaboli@gmail.com>
2025-05-02 10:31:00 +0530

    Update id_ex.vhd

--- pipelined_cpu/id_ex.vhd
+++ pipelined_cpu/id_ex.vhd
@@ -6,38 +6,38 @@ entity ID_EX_Buffer is
   port (
     clk             : in  std_logic;
     rst             : in  std_logic;
-    reg_data1_in    : in  std_logic_vector(15 downto 0);
-    reg_data2_in    : in  std_logic_vector(15 downto 0);
+    rs_data_in    : in  std_logic_vector(15 downto 0);
+    rt_data_in    : in  std_logic_vector(15 downto 0);
     imm_in          : in  std_logic_vector(15 downto 0);
     alu_op_in       : in  std_logic_vector(2 downto 0);
     dest_reg_in     : in  std_logic_vector(2 downto 0);
     
-    reg_data1_out   : out std_logic_vector(15 downto 0);
-    reg_data2_out   : out std_logic_vector(15 downto 0);
+    rs_data_out   : out std_logic_vector(15 downto 0);
+    rt_data_out   : out std_logic_vector(15 downto 0);
     imm_out         : out std_logic_vector(15 downto 0);
     alu_op_out      : out std_logic_vector(2 downto 0);
     dest_reg_out    : out std_logic_vector(2 downto 0)
   );
 end ID_EX_Buffer;
 
-architecture rtl of ID_EX_Buffer is
+architecture beh of ID_EX_Buffer is
 begin
   process(clk)
   begin
     if rising_edge(clk) then
       if rst = '1' then
-        reg_data1_out <= (others => '0');
-        reg_data2_out <= (others => '0');
+        rs_data_out <= (others => '0');
+        rt_data_out <= (others => '0');
         imm_out <= (others => '0');
         alu_op_out <= (others => '0');
         dest_reg_out <= (others => '0');
       else
-        reg_data1_out <= reg_data1_in;
-        reg_data2_out <= reg_data2_in;
+        rs_data_out <= rs_data_in;
+        rt_data_out <= rt_data_in;
         imm_out <= imm_in;
         alu_op_out <= alu_op_in;
         dest_reg_out <= dest_reg_in;
       end if;
     end if;
   end process;
-end rtl;
+end beh;
