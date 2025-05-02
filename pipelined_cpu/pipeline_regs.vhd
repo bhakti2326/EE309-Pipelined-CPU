0afa7a769e399d96f925412bdcfcdb9661d8718d
Aboli1512 <malshikareaboli@gmail.com>
2025-05-02 11:55:57 +0530

    Update pipeline_regs.vhd

--- pipelined_cpu/pipeline_regs.vhd
+++ pipelined_cpu/pipeline_regs.vhd
@@ -32,8 +32,27 @@ component IF_ID_Buffer is
 end IF_ID_Buffer;
 
 
+component ID_EX_Buffer is
+  port (
+    clk             : in  std_logic;
+    rst             : in  std_logic;
+    rs_data_in    : in  std_logic_vector(15 downto 0);
+    rt_data_in    : in  std_logic_vector(15 downto 0);
+    imm_in          : in  std_logic_vector(15 downto 0);
+    alu_op_in       : in  std_logic_vector(2 downto 0);
+    dest_reg_in     : in  std_logic_vector(2 downto 0);
+    
+    rs_data_out   : out std_logic_vector(15 downto 0);
+    rt_data_out   : out std_logic_vector(15 downto 0);
+    imm_out         : out std_logic_vector(15 downto 0);
+    alu_op_out      : out std_logic_vector(2 downto 0);
+    dest_reg_out    : out std_logic_vector(2 downto 0)
+  );
+end ID_EX_Buffer;
 
-	
+
+
+
 begin
     process(clk)
     begin
